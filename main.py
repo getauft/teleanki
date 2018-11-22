73be5aa698c7211c8d3636132fab0c577a1354ec
getauft <getauft@gmail.com>
2018-11-22 20:07:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,13 +24,10 @@ def send_mess(chat, text):
     response = requests.post(url + 'sendMessage', data=params)
     return response
 
-def main():  
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-            send_mess(get_chat_id(last_update(get_updates_json(url))), 'This is update ID ' + update_id)
-            update_id += 1
-        sleep(1)       
-
-if __name__ == '__main__':  
-    main()
+ 
+update_id = last_update(get_updates_json(url))['update_id']
+while True:
+    if update_id == last_update(get_updates_json(url))['update_id']:
+        send_mess(get_chat_id(last_update(get_updates_json(url))), 'This is update ID ' + update_id)
+        update_id += 1
+    sleep(1)       
