f3c2a5a2e92aef7416551453d33f5edea921f128
Anton Lavrinenko <getauft@gmail.com>
2018-12-09 21:37:41 +0400

    #15

--- wooordhunt.py
+++ wooordhunt.py
@@ -20,6 +20,7 @@ def translate_word(word, lang):
     result = {'transcription': transcription, 'context': context[0], 'russian': russian, 'word_forms': word_forms}
     if(lang == 'en'):
         words = word.split(' ')
+        mp3 = b''
         for w in words:
             response = urllib.request.urlopen('https://wooordhunt.ru/data/sound/word/us/mp3/' + w.lower().strip() + '.mp3')
             if(response.headers['Content-Type'] == 'audio/mpeg'):
