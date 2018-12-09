a8d0bdb979aee37148099cb52fcc84f798dcebdb
getauft <getauft@gmail.com>
2018-12-09 14:44:57 +0400

    Update wooordhunt.py

--- wooordhunt.py
+++ wooordhunt.py
@@ -16,7 +16,7 @@ def translate_word(word, lang):
     )).json()['extract']
     russian = response['translation'] + '; ' + '; '.join(response['synonyms'])
     result = {'transcription' : transcription, 'context' : context[0], 'russian' : russian, 'word_forms' : word_forms}
-    if(lang == 'en'):
+    if(lang == 'enzz'):
         response = urllib.request.urlopen('https://wooordhunt.ru/data/sound/word/us/mp3/' + word.replace(' ', '%20').lower() + '.mp3')
         if(response.headers['Content-Type'] == 'audio/mpeg'):
             mp3 = response.read()
